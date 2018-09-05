b89b4217e3904df9030b7f5be798996349e73e78
alexanderchao123 <chao.alexander.123@gmail.com>
2018-09-05 05:00:56 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(seconds)
   number = seconds
   while number > 0
     puts "#{number} SECOND(S)!"
-    countdown_with_sleep
+    # countdown_with_sleep
     number -= 1
   end
   puts "HAPPY NEW YEAR!"
